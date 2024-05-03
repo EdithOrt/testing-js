639a1a07f221ca0bfa083f3a575faf9261b8d3c5
EdithOrt <eoc.diseno@gmail.com>
2024-05-03 13:57:46 -0600

    [UPDATE] create a set of tests

---
+++ demos/src/05-setup.test.js
@@ -0,0 +1,19 @@
+describe('set describe', () => {
+  test('case 1', () => {
+    expect(1+1).toBe(2)
+  })
+
+  test('case 2', () => {
+    expect(2+2).toBe(4)
+  })
+
+  describe('other group', () => {
+    test('case 3', () => {
+      expect(3+3).toBe(6)
+    })
+
+    test('case 4', () => {
+      expect(4+4).toBe(8)
+    })
+  });
+});
