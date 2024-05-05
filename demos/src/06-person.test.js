d41cb203793140ca20119b9b1cec5465a98a91e5
EdithOrt <eoc.diseno@gmail.com>
2024-05-05 10:08:13 -0600

    [UPDATE] AAA metodology comments

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,22 +2,26 @@ const Person = require("./06-person")
 
 describe('Test person', () => {
   let person
-
+// Arrange
   beforeEach(() => {
    person = new Person("Emily", 47, 1.70)
   })
 
   test('should return down', () => {
+    // Arrange
     person.weight = 45
+    // Act
     const IMC = person.calcIMC()
-
+    // Assert
     expect(IMC).toBe("down")
   });
 
   test('should return normal', () => {
+    // Arrange
     person.weight = 59
+    // Act
     const IMC = person.calcIMC()
-
+// Assert
     expect(IMC).toBe("normal")
   });
 });
