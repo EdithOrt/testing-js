718bfc8869412b0e36c01369d7fe51bd527d48b7
EdithOrt <eoc.diseno@gmail.com>
2024-05-07 13:49:21 -0600

    [UPDATE] add mock test

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,5 +1,21 @@
 const BooksService = require("./books.service")
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: "Harry Potter"
+  }
+]
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks] ,
+  create: () => {}
+}
+
+jest.mock("../lib/mongo.lib", () => {
+  return jest.fn().mockImplementation(() => MongoLibStub)
+})
+
 describe('Tests for book service', () => {
   let service;
 
@@ -16,7 +32,7 @@ describe('Tests for book service', () => {
     const books = await service.getBooks({})
     console.log(books)
     // Assert
-    expect(books.length).toEqual(2)
+    expect(books.length).toEqual(1)
     });
   });
 });
